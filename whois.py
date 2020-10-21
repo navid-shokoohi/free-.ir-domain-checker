28fe1ce9a03d85c89816b20f0e6ebf4745a03019
navid-shokoohi <navid.shokoohi@live.com>
2020-10-21 11:44:26 +0330

    Update whois.py

--- whois.py
+++ whois.py
@@ -2,7 +2,7 @@ import sys
 import re
 import urllib.request
 
-response = urllib.request.urlopen("http://whois.nic.ir/?name=Shokoohi.ir")
+response = urllib.request.urlopen("http://whois.nic.ir/?name=DOMAIN-NAME.ir")
 for line in response:
     if "no entries found".encode() in line:
         print(line)
